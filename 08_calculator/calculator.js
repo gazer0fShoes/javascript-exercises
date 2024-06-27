75043a7c0e71e4c924b3468ae3134ff5e1b54f0d
gazer0fShoes <oisin.weitzman@gmail.com>
2024-06-27 15:56:24 +0900

    finish 08_calculator
    til the factorial of 0 is 1.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce( (total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce( (total, value) => total * value, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let answer = 1;
+  for (i = 2; i <= a; i++) {
+    answer *= i;
+  } 
+  return answer;
 };
 
 // Do not edit below this line
