b1c7bf22966696c449b87b47bdecb77181b6f725
gazer0fShoes <oisin.weitzman@gmail.com>
2024-07-01 11:26:03 +0900

    finish 12_findTheOldest
    til {} functions ALWAYS need to return something

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.sort((a, b) => {
+        if (!a.yearOfDeath) return ((b.yearOfDeath - b.yearOfBirth) - (new Date().getFullYear() - a.yearOfBirth));
+        else if (!b.yearOfDeath) return ((new Date().getFullYear() - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth))
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+    })[0];
 };
 
 // Do not edit below this line
