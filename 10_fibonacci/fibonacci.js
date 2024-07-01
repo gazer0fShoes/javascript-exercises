e61495bf518d1cd143ec36e7597ef1865c17c1d1
gazer0fShoes <oisin.weitzman@gmail.com>
2024-07-01 10:56:39 +0900

    finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let n = Number(number)
+    if (typeof n != "number" || n < 0) return "OOPS"
+    if (n === 0) return n;
 
+    let fibonacci = [1, 1];
+    for (let i = 0; i < n - 2; i++) {
+        fibonacci.push(fibonacci[i] + fibonacci[i + 1])
+    }
+    return fibonacci[n - 1];
 };
 
 // Do not edit below this line
