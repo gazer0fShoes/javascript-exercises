44c13262a8b0ec6f6c409144c443c9bdf876af3b
gazer0fShoes <oisin.weitzman@gmail.com>
2024-07-01 10:10:04 +0900

    finish 09_palindromes
    til "===" functions differently for primitive and non-primitive types

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const inputArray = Array.from(str.toLowerCase()).filter(
+        letter => (letter >= "a" && letter <= "z") || (letter >= "0" && letter <= "9"));
+    if (inputArray.join("") === inputArray.toReversed().join("")) return true;
+    return false;
 };
 
 // Do not edit below this line
